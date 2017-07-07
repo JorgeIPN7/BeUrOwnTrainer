159dde6027f512368eed5db3d17fde941b7315a5
Jorge H <jorge.ipn.7@gmail.com>
2017-07-07 15:02:58 -0500

    MainActivity [Disenio]

---
+++ app/src/main/java/jorgeipn7/com/beurowntrainer/activities/MainActivity.java
@@ -0,0 +1,144 @@
+package jorgeipn7.com.beurowntrainer.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import jorgeipn7.com.beurowntrainer.R;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+
+
+
+    Button btn_comenzar,
+    btn_editar_rutina,
+    btn_progreso,
+    btn_avances,
+    btn_accesorios;
+    TextView tv_mensaje_pp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        inicializarElementos();
+
+    }
+
+    private void inicializarElementos(){
+        btn_comenzar= (Button) findViewById(R.id.btn_comenzar);
+        btn_editar_rutina= (Button) findViewById(R.id.btn_editar_rutina);
+        btn_progreso= (Button) findViewById(R.id.btn_progreso);
+        btn_avances= (Button) findViewById(R.id.btn_avances);
+        btn_accesorios= (Button) findViewById(R.id.btn_accesorios);
+        tv_mensaje_pp= (TextView) findViewById(R.id.tv_mensaje_pp);
+
+
+        btn_comenzar.setOnClickListener(this);
+        btn_editar_rutina.setOnClickListener(this);
+        btn_progreso.setOnClickListener(this);
+        btn_avances.setOnClickListener(this);
+        btn_accesorios.setOnClickListener(this);
+
+    }
+
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_editar_rutina:
+                startActivity(new Intent(MainActivity.this, EditarRutina.class));
+                break;
+        }
+    }
+}
